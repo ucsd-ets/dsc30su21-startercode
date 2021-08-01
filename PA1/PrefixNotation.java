411c3629af662afc447c6f6f6facc4ea8818632a
Aaron Fraenkel <aaron.fraenkel@gmail.com>
2021-08-01 08:45:27 -0700

    initial commit

---
+++ PA1/PrefixNotation.java
@@ -0,0 +1,17 @@
+/*
+    Name: TODO
+    PID:  TODO
+ */
+
+/**
+ * TODO
+ * @author TODO
+ * @since  TODO
+ */
+public class PrefixNotation {
+
+    public static int evaluate(String notation) {
+        /* TODO */
+        return 0;
+    }
+}
---
+++ PA1/WeatherMonitor.java
@@ -0,0 +1,23 @@
+/*
+    Name: TODO
+    PID:  TODO
+ */
+
+/**
+ * TODO
+ * @author TODO
+ * @since  TODO
+ */
+public class WeatherMonitor {
+    
+    IntStack temps;
+    
+    public WeatherMonitor() {
+        /* TODO */
+    }
+    
+    public int numDays(int temp) {
+        /* TODO */
+        return 0;
+    }
+}
