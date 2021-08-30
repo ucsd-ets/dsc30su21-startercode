1be0268a3a98d0aeec236db438cda6e1442bc3b1
Zirui Wang <ziruiw2000@gmail.com>
2021-08-30 16:49:32 -0700

    startercode ready

---
+++ PA5/ContactList.java
@@ -0,0 +1,38 @@
+import java.util.*;   
+
+public class ContactList {
+	
+	// Add instance variables here
+	
+    public boolean createContact(Person person) {
+        return true
+    }
+
+    public boolean lookupContact(String name) {
+        return true;
+    }
+
+    public Person getContact(String name) {
+        return null;
+    }
+
+    public Person[] getContactByRange(String start, String end) {
+        return null;
+    }
+
+    public boolean deleteContact(String name) {
+        return true;
+    }
+
+    public int size() {
+        return 42;
+    }
+
+    public String[] fetchAllNames() {
+        return null;
+    }
+
+    public String[] fetchAllPhoneNumbers() {
+        return null;
+    }
+}
---
+++ PA5/Person.java
@@ -0,0 +1,26 @@
+import java.util.*;   
+
+public class Person {
+	
+    // Add instance variables here
+	
+	public Person(String name, ArrayList<String> pnArray) {
+
+	}
+	
+    public String getName() {
+        return null;
+    }
+
+    public boolean addPhoneNumber(String pn) {
+        return true;
+    }
+
+    public ArrayList<String> getPhoneNumbers() {
+        return null;
+    }
+
+    public boolean deletePhoneNumber(String pn) {
+        return true;
+    }
+}
